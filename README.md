b44af00aaa63646678153a7914bd22abe9291781
cyng93 <cyng93@gmail.com>
2016-11-25 00:25:16 +0800

    try CoffeeScript & Ajax

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # README
 
 Default toturial of Rails
+
+1. Working with JavaScript in Rails[^1]
+
+[^1]:http://guides.rubyonrails.org/working_with_javascript_in_rails.html
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,8 +4,20 @@ class CommentsController < ApplicationController
 
   def create
     @article = Article.find(params[:article_id])
-    @comment = @article.comments.create(comment_params)
-    redirect_to article_path(@article)
+    @comment = @article.comments.new(comment_params)
+
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to article_path(@article) }
+        format.js   {}
+        format.json { render json: @comment, status: :created,
+                                             location: @comment }
+      else
+        format.html { redirect_to article_path(@article) }
+        format.json { render json: @comment.errors,
+                                   status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
